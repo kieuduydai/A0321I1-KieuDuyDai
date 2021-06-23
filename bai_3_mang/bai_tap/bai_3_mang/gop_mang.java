85c10a8029f0b8dab418e77c3c354ed0e16dcd57
Kieu Duy Dai <kieuduydai1@gmail.com>
2021-06-23 20:46:52 +0700

    update

--- bai_3_mang/bai_tap/bai_3_mang/gop_mang.java
+++ bai_3_mang/bai_tap/bai_3_mang/gop_mang.java
@@ -1,12 +1,4 @@
 package bai_3_mang;
 import java.util.Arrays;
 public class gop_mang {
-	static void m(int x) { 
-		  x = 200; 
-		} 
-	public static void main(String[] args) {
-		int a = 100; 
-		  m(a); 
-		  System.out.println(a); 
-	}	
 }
--- bai_3_mang/bai_tap/bai_3_mang/them_pt.java
+++ bai_3_mang/bai_tap/bai_3_mang/them_pt.java
@@ -1,5 +1,37 @@
 package bai_3_mang;
-
+import java.util.Scanner;
 public class them_pt {
-
+	public static Scanner sc = new Scanner(System.in);
+	public static void ramdom(int a[],int n) {
+		int i;
+		for(i = 0; i < n; i++)
+			a[i] = (int) (Math.random() * 100);
+	}
+	
+	public static void out(int a[],int n) {
+		int i;
+		for(i = 0; i < n; i++)
+			System.out.print(a[i]+" ");
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.print("Nhap so phan tu:");
+		int n = sc.nextInt();
+		int []a = new int [n];
+		ramdom(a, n);
+		out(a,n);
+		System.out.print("\nNhap phan tu k:");
+		int k = sc.nextInt();
+		
+		System.out.printf("Nhap vi tri :");
+		int x = sc.nextInt();
+		
+		
+        System.out.print("\nMang sau khi chen: ");
+        out(a,n);
+        
+	}
 }
