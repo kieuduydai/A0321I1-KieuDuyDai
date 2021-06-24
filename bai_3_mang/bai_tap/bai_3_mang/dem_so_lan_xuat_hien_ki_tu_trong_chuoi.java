1ac3fb8f38b450d70978b02bf67e8e8b41730c73
Kieu Duy Dai <kieuduydai1@gmail.com>
2021-06-24 19:56:48 +0700

    update

--- bai_3_mang/bai_tap/bai_3_mang/dem_so_lan_xuat_hien_ki_tu_trong_chuoi.java
+++ bai_3_mang/bai_tap/bai_3_mang/dem_so_lan_xuat_hien_ki_tu_trong_chuoi.java
@@ -1,5 +1,25 @@
 package bai_3_mang;
+import java.util.Scanner;
 
 public class dem_so_lan_xuat_hien_ki_tu_trong_chuoi {
-
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+        System.out.print("Nhap chuoi:");
+        String string = sc.nextLine();
+        
+        System.out.print("Bạn nhập kí tự cần kiểm tra");
+        char k = sc.nextLine().charAt(0);
+        
+        int count=0;
+        
+        for (int i = 0;i < string.length(); i++)
+            if(k == string.charAt(i))
+                count++;
+            
+        
+        System.out.println("So lan xuat hien cua "+k+" la "+count+" lan");
+	}
 }
+
