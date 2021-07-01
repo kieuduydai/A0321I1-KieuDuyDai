57e7354cc572f232e8ba7fca8e134e0178bb2c58
Kieu Duy Dai <kieuduydai1@gmail.com>
2021-07-02 04:07:08 +0700

    nop bai

--- bai_6_ke_thua/src/bai_tap/Point2D.java
+++ bai_6_ke_thua/src/bai_tap/Point2D.java
@@ -1,5 +1,35 @@
 package bai_tap;
 
 public class Point2D {
-
+	private float x = 0.0f;
+	private float y = 0.0f;
+	
+	public Point2D() {
+	
+	}
+	
+	public Point2D(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public float getX() {
+		return this.x;
+	}
+	
+	public void setX(float x) {
+		this.x = x;
+	}
+	
+	public float getY() {
+		return this.y;
+	}
+	
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	public String toString() {
+		return "(x,y) = "+"("+ this.x + "," + this.y;
+	}
 }
--- bai_6_ke_thua/src/bai_tap/Point3D.java
+++ bai_6_ke_thua/src/bai_tap/Point3D.java
@@ -1,5 +1,34 @@
 package bai_tap;
 
 public class Point3D extends Point2D {
+	private float z ;
 	
+	public Point3D() {
+	
+	}
+	
+	public Point3D(float x, float y , float z) {
+		super(x,y);
+		this.z = z;
+	}
+	
+	public float getZ() {
+		return this.z;
+	}
+	
+	public void setZ(float z) {
+		this.z = z;
+	}
+	
+	public String toString() {
+		return "(x,y,z) = "+"(" + super.getX() + "," + super.getY() + "," + this.z + ")"; 
+	}
+	
+	public static void main(String[] args) {
+		Point3D obj = new Point3D(1,2,3);
+		System.out.print(obj);
+		
+		Point2D obj1 = new Point2D(2,1);
+		System.out.println("\n"+obj1+")");
+	}
 }
