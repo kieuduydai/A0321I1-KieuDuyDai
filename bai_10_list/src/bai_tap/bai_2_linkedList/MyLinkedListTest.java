618da07c70d7c2f2be2963e8b82adec45522b16b
Kieu Duy Dai <kieuduydai1@gmail.com>
2021-08-01 19:41:09 +0700

    nop tre

---
+++ bai_10_list/src/bai_tap/bai_2_linkedList/MyLinkedListTest.java
@@ -0,0 +1,24 @@
+package bai_tap.bai_2_linkedList;
+
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.addFirst(6);
+        myLinkedList.addFirst(7);
+        myLinkedList.addLast(3);
+        myLinkedList.add(1, 8);
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.println("element " + i + ": " + myLinkedList.get(i));
+        }
+        myLinkedList.remove(2);
+        System.out.println("after remove");
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.println("element " + i + ": " + myLinkedList.get(i));
+        }
+        MyLinkedList<Integer> clone = myLinkedList.clone();
+        System.out.println("Array clone: ");
+        for (int i = 0; i < clone.size(); i++) {
+            System.out.println("element " + i + ": " + clone.get(i));
+        }
+    }
+}
